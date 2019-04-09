724aa774e7924c736b49966314535eac232c0887
Pavlo Ptushkin <pavel0131@gmail.com>
2019-04-09 15:34:38 -0400

    name prompt

--- RPS/rpsFunction.js
+++ RPS/rpsFunction.js
@@ -12,6 +12,7 @@ var player2 = {firstName:"Jane", getHand: getHand, hand:null, wins:playerWins};
 var player3 = {firstName:"Joe", getHand: getHand, hand:null, wins:playerWins};
 var player4 = {firstName:"Jill", getHand: getHand, hand:null, wins:playerWins};
 function playRound(player1, player2){
+    player1.hand = this.prompt("what is your hand").toLowerCase();
     player2.hand = player2.getHand();
     if (player1.hand === player2.hand)
     {
@@ -39,7 +40,6 @@ function playGame (player1, player2, playUntil)
 {
     while (player1.wins < playUntil && player2.wins < playUntil)
     {
-        player1.hand = this.prompt("what is your hand").toLowerCase();
         playRound(player1,player2);
     }
 }
@@ -66,6 +66,7 @@ function playTournament(player1, player2, player3, player4, playUntil)
     if (game1Winner.wins == playUntil) return game1Winner.firstName + " is the world champion"; else return game2Winner.firstName + " is the world champion";  
 }
 function myFunction(){
+player1.firstName = this.prompt("what is your name").toUpperCase();
 var playUntil = prompt("How many games will the computers play?")
 document.querySelector('#w1').textContent = playTournament(player1,player2,player3,player4, playUntil)
 document.querySelector('button').style.display = 'none';
