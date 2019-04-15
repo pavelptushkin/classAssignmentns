50ff1731c5a26fd4468710e3f52972c504ff6bb2
Pavlo Ptushkin <pavel0131@gmail.com>
2019-04-15 16:35:25 -0400

    littleboy

--- Lesson-7/exercise3.js
+++ Lesson-7/exercise3.js
@@ -6,42 +6,39 @@ var woolOwners = [
         "dame": 1
     },
     {
-      "littleBoy": 1,
+      "little boy": 1,
       "location": "down the lane"
     }
   ];
   
-  
-  var bags;
+  var bags = 0;
   var haveYouAnyWool = function() {
       for (var i = 0; i < woolOwners.length; i++) {
-      bags += i;
+      bags = bags + i;
       }
       return bags;
   };
   haveYouAnyWool();
-  
-  
   function baabaaBlackSheep() {
       console.log("BaaBaa BlackSheep have you any wool?");
       if (bags > 0) {
-          console.log("yes sir, yes sir " + totalBags + " bags full");
+          console.log("yes sir, yes sir " + bags + " bags full");
     }
   }
   
   function oneForMy() {
       for (var i = 0; i < 2; i++) {
-          people = Object.keys(woolOwners);
+          people = Object.keys(woolOwners[i]);
           var person = people.toString();
-          console.log("one for my " + person);
+          console.log("one for my " + person)
       }
   }
   
   baabaaBlackSheep();
   oneForMy();
   
-  var boy = Object.keys(woolOwners[2]);
-  var littleBoy = boy[2];
+  var boy = woolOwners[2];
+  var littleBoy = Object.keys(woolOwners[2]);
   
   var whereHeLives = woolOwners.location;
-  console.log("one for the " + woolOwners.littleBoy + " that lives " + whereHeLives);
+  console.log("one for the " + littleBoy[0] + " that lives " + boy.location);
